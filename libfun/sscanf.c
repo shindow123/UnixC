2bd7a358f8787dc59d82bb371a00fdde9f84072f
deancheng <deancheng@taomee.com>
2016-04-28 15:13:42 +0800

    sscanf使用

--- libfun/sscanf.c
+++ libfun/sscanf.c
@@ -6,7 +6,8 @@ int main(){
     char text[256];
     long arg1, arg2;
     while (fgets(text, 256, stdin) > 0) {
-        if (sscanf(text, "%ld%ld", &arg1, &arg2) == 2) {
+        // "%ld%ld" 是以空格分开 "%ld:%ld" 以:分开
+        if (sscanf(text, "%ld %ld", &arg1, &arg2) == 2) {
             printf("%ld,%ld\n", arg1, arg2);
         } else {
             printf("error:%s\n", strerror(errno));
